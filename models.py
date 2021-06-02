9f2a68303bdddd13c2beb9dd0759fcdaf7e17185
Bredell_Evans <bredy452@gmail.com>
2021-06-02 05:38:55 -0400

    getting client schedule route working

--- models.py
+++ models.py
@@ -11,8 +11,8 @@ DATABASE = connect(os.environ.get('DATABASE_URL') or 'sqlite:///booking.sqlite')
 
 class Org_user(UserMixin, Model):
 	org_name = CharField(unique=True)
-	first_name = CharField(null=False)
-	last_name = CharField(null=False)
+	first_name = CharField(null=True)
+	last_name = CharField(null=True)
 	email = CharField(null=False)
 	username = CharField(null=False)
 	password = CharField(null=False)
@@ -25,7 +25,7 @@ class Org_user(UserMixin, Model):
 class Client(UserMixin, Model):
 	first_name = CharField(null=False)
 	last_name = CharField(null=False)
-	phone_number = CharField()
+	phone_number = CharField(null=False)
 	username = CharField(null=False)
 	email = CharField(null=False)
 	password = CharField(null=False)
@@ -34,27 +34,27 @@ class Client(UserMixin, Model):
 	class Meta:
 		database = DATABASE
 
-# class Schedule(Model):
-# 	info= CharField()
-# 	org_id = ForeignKeyField(Org_user, backref='schedule')
-# 	date = CharField()
-# 	client_id = ForeignKeyField(Client, backref='my_schedule')
+class Client_schedule(Model):
+	client_availability = CharField()
+	client_id = ForeignKeyField(Client, backref='my_schedule', null=True)
+	org_id = ForeignKeyField(Org_user, backref='schedule')
 
-# 	class Meta:
-# 		database = DATABASE
+	class Meta:
+		database = DATABASE
 
 class Schedule(Model):
 	availability = CharField()
 	# info = CharField()
 	org_id = ForeignKeyField(Org_user, backref='schedule')
 	# date = CharField()
-	# client_id = ForeignKeyField(Client, backref='my_schedule')
+	# client_schedule_id = ForeignKeyField(Client_schedule, backref='client_schedule', null=True)
 
 	class Meta:
 		database = DATABASE
 
+
 def initialize():
 	DATABASE.connect()
-	DATABASE.create_tables([Org_user, Client, Schedule], safe=True)
+	DATABASE.create_tables([Org_user, Client, Schedule, Client_schedule], safe=True)
 	print("Connected to the DB and ready to rumble")
 	DATABASE.close()
